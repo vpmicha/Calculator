dc59bfbedd1dc55f71ad8577f8f26ae4b8c5c253
vpmicha <vpmicha@gmail.com>
2026-08-17 03:49:55 +0300

    bufixing

--- calculator.py
+++ calculator.py
@@ -4,34 +4,26 @@ def main():
         if '+' in ask:
             print(addition(ask))
             break
-        if '-' in ask:
+        elif '-' in ask:
             print(subtraction(ask))
             break
-        if '*' in ask:
+        elif '*' in ask:
             print(multiplication(ask))
             break
-        if '/' in ask:
+        elif '/' in ask:
             print(division(ask))
             break
 def addition(numbers):
-    numbers_list = []
     try:
-        if numbers.count('+') > 1:
-            numbers_list.append(numbers.split('+'))
-            return numbers_list[1] - numbers_list[2]
-        first, last = map(int, numbers.split('+'))
+        first, last = map(int, numbers.rsplit('+', 1))
     except ValueError:
         raise ValueError('Wrong Format')
 
     return first + last
 
 def subtraction(numbers):
-    numbers_list = []
     try:
-        if numbers.count('-') > 1:
-            numbers_list.append(numbers.split('-'))
-            return numbers_list[1] - numbers_list[2]
-        first, last = map(int, numbers.split('-'))
+        first, last = map(int, numbers.rsplit('-', 1))
     except ValueError:
         raise ValueError('Wrong Format')
 
@@ -39,7 +31,7 @@ def subtraction(numbers):
 
 def multiplication(numbers):
     try:
-        first, last = map(int, numbers.split('*'))
+        first, last = map(int, numbers.rsplit('*', 1))
     except ValueError:
         raise ValueError('Wrong Format')
 
@@ -47,14 +39,12 @@ def multiplication(numbers):
 
 def division(numbers):
     try:
-        first, last = map(int, numbers.split('/'))
-        answer = first / last
+        first, last = map(int, numbers.rsplit('/', 1))
+        return first / last
     except ValueError:
         raise ValueError('Wrong Format')
     except ZeroDivisionError:
         raise ZeroDivisionError("Can't divide by 0")
-
-    return answer
 
 if __name__ == '__main__':
     main()
